c63066a572d82b6e0f1cbbabea1440f6ca4c59b1
srjahir32 <support@jacksolutions.biz>
2016-07-13 17:04:50 +0530

    removing unwanted files

--- config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'connection': {
-        'host': '127.0.0.1',
-        'user': 'admin1GQSkCB',
-        'password': '9KiRXVYVkKpU'
-    },
-	'database': 'nodejs',
-    'users_table': 'client_login'
-};
